d624c793def740d7d17b438de4f26266ed3da37f
Dinobi <architectstan01@gmail.com>
2022-10-15 12:56:21 -0700

    gb

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,11 +16,10 @@ int main(void)
 	ldigit = n % 10;
 	if (ldigit > 5)
 {	printf("Last digit of %i is %i and is greater than 5\n", n, ldigit);
-
-}	
+}
 	else if (ldigit < 6 && ldigit != 0)
 {	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ldigit);
-}	
+}
 	else if (ldigit == 0)
 {	printf("Last digit of %i is %i and is 0\n", n, ldigit);
 }	return (0);
