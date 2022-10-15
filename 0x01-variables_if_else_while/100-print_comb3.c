a91981ed855d80a2c189e0188e8c28cf6028335a
Dinobi <architectstan01@gmail.com>
2022-10-15 15:11:33 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 success,non zero fail
+ */
+int main(void)
+{
+	int a, b = '0', c;
+
+	for (a = '0'; a <= '9'; a++)
+{	for (c = b; c <= '9'; c++)
+{	if (a != c)
+{	putchar(a);
+	putchar(c);
+}
+	if (a == c)
+{	continue;
+}
+	if (a == '8' && c == '9')
+{	break;
+}
+	else
+{	putchar(',');
+	putchar(' ');
+}
+}
+	b++;
+}
+	putchar('\n');
+	return (0);
+}
