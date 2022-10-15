00a50fad4c547cc1d5e3ba3f2a5f0f530cdc41de
Dinobi <architectstan01@gmail.com>
2022-10-15 13:45:22 -0700

    alp

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * main-program entry point.
+ * Return:0 if no error, non error if error.
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a' x <= 'z'; x++)
+{	if (x == 'e' || x == 'q')
+	continue;
+	putchar(x);
+}
+	putchar('\n');
+	return (0);
+}
