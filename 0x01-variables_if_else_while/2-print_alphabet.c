090469e9be40af538ebd37663e943ad9b3213bbd
Dinobi <architectstan01@gmail.com>
2022-10-15 13:13:55 -0700

    Alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main-program entry point.
+ * Return:0 if no error, non zero if error.
+ */
+int main(void)
+{
+	char x;
+
+	for(i = 'a'; x <= 'z'; x++)
+{	putchar(x);
+}
+	putchar("\n");
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-readme
+real
