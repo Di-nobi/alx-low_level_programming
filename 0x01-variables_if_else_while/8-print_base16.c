c91168cdf0b31c2a3e1a28960ef2dd9427a46cc8
Dinobi <architectstan01@gmail.com>
2022-10-15 14:28:45 -0700

    base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-program entry point.
+ *Return:0 if no error, non zeroif error.
+ */
+int main (void)
+{
+	int x;
+	charhexvalues[] = "0123456789abcdef";
+
+	for (x = 0; x < 16; x++)
+{	putchar(hexvalues[x]);
+}
+	putchar('\n');
+	return (0);
+}
