fd91fb2e04e23d651639c71cbe2f784321cafb91
Dinobi <architectstan01@gmail.com>
2022-10-15 14:11:51 -0700

    6 prf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-program entry point.
+ *Return:0 no error, non zeroif error
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+	putchar('\n');
+	return(0);
+}
