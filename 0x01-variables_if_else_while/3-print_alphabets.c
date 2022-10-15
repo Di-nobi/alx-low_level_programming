cd0ec8350d31c3cb0beb487be257215d2cb3037f
Dinobi <architectstan01@gmail.com>
2022-10-15 13:31:34 -0700

    bet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-program entry point.
+ *Return:0 no error, non zero if error.
+ **/
+int main(void)
+{
+	char x;
+	char y;
+	for (x = 'a'; x <= 'z'; x++)
+{	putchar(x);
+}
+	for (y = 'A'; y <= 'Z'; y++)
+{	putchar(y);
+}
+	putchar('\n');
+	return (0);
+}
