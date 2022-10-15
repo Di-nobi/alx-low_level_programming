951b745f26953648d609302cb1315e2d5b307da2
Dinobi <architectstan01@gmail.com>
2022-10-15 15:47:36 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main-program entry point.
+ *
+ * Description: prints all possible combinations of two two-digit numbers
+ *
+ * Return:0 success, non zero fail
+ */
+int main(void)
+{
+	int a,b;
+
+	for (a = 0; a < 99; a++)
+{
+	for (b = a + 1; b <= 99; b++)
+{
+	putchar('0' + a / 10);
+	putchar('0' + a % 10);
+
+	putchar(' ');
+
+	putchar('0' + b / 10);
+	putchar('0' + b % 10);
+
+	if (a == 98 && b == 99)
+		break;
+	putchar(',');
+	putchar(' ');
+}
+}
+	putchar('\n');
+	return (0);
+}
