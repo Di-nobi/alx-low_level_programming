1c8f0079a7f5958393c94ba58f3ed0e4e9003e88
Dinobi <architectstan01@gmail.com>
2022-10-15 13:50:54 -0700

    4pf

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 /*
- * main-program entry point.
- * Return:0 if no error, non error if error.
+ *main-program entry point.
+ *Return:0 if no error, non error if error.
  */
 int main(void)
 {
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
-{	if (x == 'e' || x == 'q')
+{
+	if (x == 'e' || x == 'q')
 	continue;
 	putchar(x);
 }
