67fb3fdc2f8a2c1f3dd493678c1002c0346eb4d8
Dinobi <architectstan01@gmail.com>
2022-10-15 01:20:37 -0700

    P/N

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%i is positive\n", n);
+ printf("%i is positive\n", n);
 }
 else if (n == 0)
 {
  printf("%i is zero\n", n);
 }
 else if (n < 0)
-printf("%i is negative\n", n);
+ printf("%i is negative\n", n);
 return (0);
 }
 
