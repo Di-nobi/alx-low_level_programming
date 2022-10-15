5f57c59569898383fdd8ec5f1727b49e5076bbd2
Dinobi <architectstan01@gmail.com>
2022-10-14 23:54:06 -0700

    P/N

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,12 @@
 /**
  *Program will assign a random number to a variable
  */
-int main (void)
+int main(void)
 {
 	int n;
+	
 	srand(time0));
-	n = rand() - srand_Max / 2;
+	n = rand() - RAND_Max / 2;
 	if (n > 0){
 	printf("%i is positive\n", n);
 	}else if (n == 0){
