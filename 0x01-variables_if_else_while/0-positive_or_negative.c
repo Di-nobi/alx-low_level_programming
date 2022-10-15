cbf537171b4515bfa0f49b67dc5be5f7cce12abf
Dinobi <architectstan01@gmail.com>
2022-10-15 02:00:18 -0700

    p/n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,11 @@ if (n > 0)
 {
  printf("%i is positive\n", n);
 }
-
-else if (n < 0)
+else if (n < 0)	
 {
  printf("%i is negative\n", n);
 }
-
-else if (n == 0)
+else if (n == 0)	
  printf("%i is zero\n", n);
 return (0);
 }
-
-
