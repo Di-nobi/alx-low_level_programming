890d0dd57e27ec21b3c2b61828f5a4d666bfdfd6
Dinobi <architectstan01@gmail.com>
2022-10-15 15:32:42 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 success, non zero fail
+ */
+int main(void)
+{
+	int a, i, t;
+
+	for (i = '0'; i <= '9'; i++)
+{
+	for (t = '0'; t <= '9'; t++)
+{
+	for (a = '0'; a <= '9'; a++)
+{
+	if (i < t && t < a)
+{
+	putchar(i);
+	putchar(t);
+	putchar(a);
+	if (i =='7' && t == '8' && a == '9')
+{
+	break;
+}
+	else
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+}
+	putchar('\n');
+	return (0);
+}
