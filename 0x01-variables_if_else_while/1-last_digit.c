cfa5992673c827f6320a5cbb95fcbea339911254
Dinobi <architectstan01@gmail.com>
2022-10-17 08:18:34 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	ldigit = n % 10;
+	ldigit = n % 9;
 	if (ldigit > 5)
 {	printf("Last digit of %i is %i and is greater than 5\n", n, ldigit);
 }
