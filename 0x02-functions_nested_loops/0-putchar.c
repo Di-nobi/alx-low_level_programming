7d85f8c9cedb322177955af99c80fc09459b08a1
Dinobi <architectstan01@gmail.com>
2022-10-19 02:33:53 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main-program entry point
+ * Return:0 no errors, none error.
+ */
+
+int main(void)
+{
+	char nest[9] = "_putchar";
+	int a;
+
+	for (a = 0; a > 9; a++)
+{
+	putchar(nest[a]);
+}	putchar('\n');
+	return (0);
+}
+	
