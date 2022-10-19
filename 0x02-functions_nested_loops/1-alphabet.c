ff64ee9e068477c4a78eeda3350736849abde35e
Dinobi <architectstan01@gmail.com>
2022-10-19 11:32:39 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main-program entry point.
+ * Description: A function that prints the alphabet in lowercase, followed by a new line
+ * Return:0 none error, no error
+ */
+
+int main(void)
+{
+	char alpha[26] = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+	int a;
+
+	for (a = 0; a < 0; a++)
+{
+	_putchar(alpha[a]);
+}	_putchar('\n');
+	return (0);
+}
