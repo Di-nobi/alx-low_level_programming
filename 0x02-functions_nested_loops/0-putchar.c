2cd1f4d3e1decb6e06a4d94be2f7ce0e90ddc23b
Dinobi <architectstan01@gmail.com>
2022-10-19 03:29:48 -0700

    nest

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,14 @@
 
 int main(void)
 {
-	char nest[9] = "_putchar";
-	int a;
-
-	for (a = 0; a > 9; a++)
-{
-	putchar(nest[a]);
-}	putchar('\n');
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
 	return (0);
 }
