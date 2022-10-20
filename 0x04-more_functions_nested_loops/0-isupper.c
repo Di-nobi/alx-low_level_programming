6424f851abaa24a60915ec0dfc2a8df760729594
Dinobi <architectstan01@gmail.com>
2022-10-20 11:30:09 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * Description: isupper - a function that checks an uppercase
+ * Return: 1 if c is uppercase otherwise return 0
+ */
+
+int _isupper(void)
+{
+	char x;
+
+	if (x = 'A')
+{
+	return (1);
+}	
+	else
+{
+	return (0);
+}
+}
