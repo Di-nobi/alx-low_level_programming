b8338bb099f46f5cc60b7bb1f2fd79a032812996
Dinobi <architectstan01@gmail.com>
2022-10-20 02:04:15 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+/**
+ * main- main block
+ * Description: Print the sum of all multiplrs of 3 or 5 below 1024.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, a;
+
+	i = 1;
+	a = 0;
+
+	for (i = 1; i < 1024; i++)
+{
+	if (i % 3 == 0)
+{
+	a += i;
+}
+	else if (i % 5 == 0)
+{
+	a += i;
+}
+}
+	printf("%i\n", total);
+	return (0);
+}
