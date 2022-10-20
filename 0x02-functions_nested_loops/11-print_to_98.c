46ef3279b1c16f5ce6804006fa4592b7c8862acb
Dinobi <architectstan01@gmail.com>
2022-10-20 00:18:47 -0700

    p

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,25 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n < 98)
 {
-	while (n > 98)
-		printf("%i, ", n--);
-	printf("%i\n", n);
-}
-	else
+	while (n <= 98)
 {
-	while (n < 98)
-		printf("%i, ", n++);
-		printf("%i\n", n);
+	printf("%i", n);
+	if (n != 98)
+{
+	printf(",");
+}	n++;
 }
+}	else
+{
+	while (n >= 98)
+{
+	printf("%i", n);
+	if (n != 98)
+{
+	printf(",");
+}	n++;
+}
+}	printf("\n");
 }
