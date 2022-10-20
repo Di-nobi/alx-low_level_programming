5b014db89b2a31e49e1fb3cc4a5b91a9a339720d
Dinobi <architectstan01@gmail.com>
2022-10-19 17:00:52 -0700

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,26 +11,25 @@ void times_table(void)
 {
 	int a, b, c;
 
-	
 	for (a = 0; a <= 9; a++)
 {
 	for (b = 0; b <= 9; a++)
 {
 	if (b == 0)
 {
-	_putchar(b + 20);
+	_putchar(b + 48);
 	continue;
 }	c = a * b;
 	_putchar(',');
 	_putchar(' ');
 	if (c >= 10)
 {
-	_putchar(c / 10 + 20);
-	_putchar(c % 10 + 20);
+	_putchar(c / 10 + 48);
+	_putchar(c % 10 + 48);
 }	else
 {
 	_putchar(' ');
-	_putchar(c + 20);
+	_putchar(c + 48);
 }
 } _putchar('\n');
 }
