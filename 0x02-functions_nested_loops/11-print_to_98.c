b138841e7709ea5009a2c576f028757dd60a4f2b
Dinobi <architectstan01@gmail.com>
2022-10-20 00:42:40 -0700

    p

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,16 +16,18 @@ void print_to_98(int n)
 	if (n != 98)
 {
 	printf(",");
+	printf(" ");
 }	n--;
 }
 }	else
 {
 	while (n <= 98)
 {
-	printf("%i ", n);
+	printf("%i", n);
 	if (n != 98)
 {
 	printf(",");
+	printf(" ");
 }	n++;
 }
 }	printf("\n");
