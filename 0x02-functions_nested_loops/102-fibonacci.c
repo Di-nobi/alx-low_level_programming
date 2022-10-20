f9e57a47b70d7d31bfca7459119e144ee6d35ff0
Dinobi <architectstan01@gmail.com>
2022-10-20 07:42:07 -0700

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Description: 102-fibonacci.c - a program that prints the first 50 fibonacci
+ * numbers, starting with 1 and 2, followed by a new line.
+ */
+
+int main(void)
+{
+	long int a, b, c, up;
+
+	a = 1;
+	b = 2;
+	c = 1;
+
+	while (c <= 50)
+{
+	if (a != 2147483647)
+{
+	printf("%li, ", a);
+}
+	else
+{
+	printf("%li\n", a);
+}
+	up = a + b;
+	a = b;
+	b = up;
+	c++;
+}
+	return (0);
+}
