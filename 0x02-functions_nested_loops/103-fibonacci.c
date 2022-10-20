5dd44b98b4745d0bf3453175c523455a6e16d3a5
Dinobi <architectstan01@gmail.com>
2022-10-20 10:28:04 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print fibonacci
+ * 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned int v, w, x, d, c;
+
+	v = 1;
+	w = 2;
+	c = 3;
+	s = 2;
+
+	d = 2;
+	while (d <= 32)
+{
+	if (c % 2 == 0)
+	s = s + c;
+	v = w;
+	w = c;
+	c = a + b;
+	d++;
+}
+	printf("%u\n", s);
+	return (0);
+}
