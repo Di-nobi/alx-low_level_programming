f940cee8d6bfbb5e7c86915b050c4c83dac08515
Dinobi <architectstan01@gmail.com>
2022-10-21 14:41:04 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - prints the numbers from 1 to 100
- * 3 multiples print Fizz instead of the number
- * 5 multiples print Buzz instead of the number
- * 3 & 5 multiples print FizzBuzz instead of the number
- * Return: Always 0 (Success)
+ * main - prints different characters for different numbers
+ * Return: 0
  */
 int main(void)
 {
-	int x;
+	int n;
 
-	const char* di0 = "Fizz";
-	const char* di1 = "Buzz";
-	const char* di2 = "FizzBuzz";
-
-	for (x = 1; x <= 100; x++)
-{
-	if (x == 100)
-	printf("%s", di1);
-	else if ((x % 3 == 0) && (x % 5 == 0))
-	printf("%s ", di2);
-	else if (x % 3 == 0)
-	printf("%s ", di0);
-	else if (x % 5 == 0)
-	printf("%s ", di1);
-	else
-	printf("%d ", x);
-}
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
 	printf("\n");
 	return (0);
 }
