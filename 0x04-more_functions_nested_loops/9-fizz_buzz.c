3b2f3a1735b15db955a2b48a277f89d4dc82f6be
Dinobi <architectstan01@gmail.com>
2022-10-21 14:38:42 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,24 +10,25 @@
  */
 int main(void)
 {
-int x;
-const char* di0 = "Fizz";
-const char* di1 = "Buzz";
-const char* di2 = "FizzBuzz";
+	int x;
 
-for (x = 1; x <= 100; x++)
+	const char* di0 = "Fizz";
+	const char* di1 = "Buzz";
+	const char* di2 = "FizzBuzz";
+
+	for (x = 1; x <= 100; x++)
 {
-if (x == 100)
-printf("%s", di1);
-else if ((x % 3 == 0) && (x % 5 == 0))
-printf("%s ", di2);
-else if (x % 3 == 0)
-printf("%s ", di0);
-else if (x % 5 == 0)
-printf("%s ", di1);
-else
-printf("%d ", x);
+	if (x == 100)
+	printf("%s", di1);
+	else if ((x % 3 == 0) && (x % 5 == 0))
+	printf("%s ", di2);
+	else if (x % 3 == 0)
+	printf("%s ", di0);
+	else if (x % 5 == 0)
+	printf("%s ", di1);
+	else
+	printf("%d ", x);
 }
-printf("\n");
-return (0);
+	printf("\n");
+	return (0);
 }
