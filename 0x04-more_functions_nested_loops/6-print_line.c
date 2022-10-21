5fe9bc9cfcdb6dfa5873e72a5552808d55e61ec1
Dinobi <architectstan01@gmail.com>
2022-10-21 13:59:41 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_line - prints an underscore n times
+ * @n: number of times printed
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+putchar('_');
+}
+putchar('\n');
+}
