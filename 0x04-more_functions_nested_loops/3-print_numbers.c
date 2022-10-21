5759a44eef8564026678ec292288f57a00e5f696
Dinobi <architectstan01@gmail.com>
2022-10-21 00:11:12 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_numbers - checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+{
+	putchar(x);
+}	putchar('\n');
+}
