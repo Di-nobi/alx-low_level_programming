e2e319a2622a3ef080a28f89daed3de5b0bcde2f
Dinobi <architectstan01@gmail.com>
2022-10-22 11:24:09 -0700

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,4 +11,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
+void print_number(int n);
+
+#endif /* MAIN_H */
