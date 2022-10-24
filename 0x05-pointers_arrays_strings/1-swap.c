565c9f7c5ea8224bf4e48aa4af04bbe9d8bb3dc6
Dinobi <architectstan01@gmail.com>
2022-10-24 04:36:52 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 1-swap.c - a function that swaps the value of two integers
+ * swap-int - a function that swaps the value of two integers
  * @a: First int
  * @b: Second int
  */
