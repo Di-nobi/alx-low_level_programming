ace5d6ca7c6048f9d8e4815b72f3eb117b31eeeb
Dinobi <architectstan01@gmail.com>
2022-10-24 05:45:29 -0700

    print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - This prints a string in reverse
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int c;
+
+	c = 0;
+	while (s[c] >= '\0')
+{	c--
+}	_putchar('\n');
+}
