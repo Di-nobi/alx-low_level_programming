fecbf11206a152e7bbfe4ab2a0d9836121078d8f
Dinobi <architectstan01@gmail.com>
2022-10-24 06:51:26 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - a function that reverse a string
+ * @s: string character
+ */
+
+void rev_string(char *s)
+{
+	char len;
+	int a, b, c;
+
+	for (a = 0; s[a] != '\0' a++)
+		b = a - 1;
+	for (c = 0; c < a / 2; c++)
+{	len = s[c];
+	s[c] = s[b];
+	s[b--] = len;
+}
+}
