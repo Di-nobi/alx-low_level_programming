145e4b294c0ce08cec426a2e29c34026cbc19918
Dinobi <architectstan01@gmail.com>
2022-10-24 06:00:47 -0700

    prr

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,12 @@ void print_rev(char *s)
 	int c, d, e;
 
 	c = 0;
-	d = e - 1;
+
 	while (s[c] <= '\0')
 {	c++;
 }	e = c;
-	while ( d >= '\0')
+	while (d = e - 1)
+	s[d] >= '\0'
 	d--;
 	
 	_putchar('\n');
