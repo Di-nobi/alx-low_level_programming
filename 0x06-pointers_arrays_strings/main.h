5a71088b19d38a9ba13739ac200d178c4cd3f011
Dinobi <architectstan01@gmail.com>
2022-10-27 20:52:09 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,4 +6,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char);
+char *infinite_add(char *n1, char *n2);
+void print_buffer(char *b, int size);
