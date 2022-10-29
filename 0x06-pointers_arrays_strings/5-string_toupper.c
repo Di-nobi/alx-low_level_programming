78ae987239877310214029abc97ccd300181b083
Dinobi <architectstan01@gmail.com>
2022-10-28 17:41:41 -0700

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+
+char *string_toupper(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+{
+	if (s[a] >= 'a' && s[i] <= 'z')
+		s[a] = s[a] - 32;
+
+
+}
+	return (s);
+}
