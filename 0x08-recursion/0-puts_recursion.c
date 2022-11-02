990534299141142a9a0cb5c4a400369e3ab51e33
Dinobi <architectstan01@gmail.com>
2022-11-02 11:18:11 -0700

    1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == NULL)
+{	
+	_putchar('\n');
+	return;
+}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
