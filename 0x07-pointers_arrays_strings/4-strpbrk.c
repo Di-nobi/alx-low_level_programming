22dbbb01c5c703c756f175cd6af97f5e4b587673
Dinobi <architectstan01@gmail.com>
2022-11-01 23:26:59 -0700

    strp

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - a function that searches a string for any set of bytes
+ * @s: string
+ * @accept: string
+ * Return: char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+{
+	for (a = 0; accept[a]; a++)
+		if (*s == accept[a])
+{
+	return (s)
+}
+	s++;
+}
+	return ('\0');
+}
