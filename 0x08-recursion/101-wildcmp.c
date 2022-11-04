bc13754264d12935f97f9ab42f039ee8f380b238
Dinobi <architectstan01@gmail.com>
2022-11-03 21:48:50 -0700

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,7 +14,8 @@ int wildcmp(char *s1, char *s2)
 	else if (*s1 == '\0' || *s2 == '\0')
 		return (0);
 	else if (*s1 == '\0' && *s2 == '*')
-		return (wildcmp(s1(s2 + 1)));
+		return (wildcmp(s1, (s2 + 1)));
+
 	if (*s1 == *s2)
 		return (wildcmp((s1 + 1), (s2 + 1)));
 	if (*s2 == '*')
