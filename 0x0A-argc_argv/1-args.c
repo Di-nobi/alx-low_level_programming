6c2c70602fcaf3da20d071b4a736809f7f6aca3f
Dinobi <architectstan01@gmail.com>
2022-11-05 02:28:02 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,10 @@
  * main - a program that display arg
  * @argc: int
  * @argv: string
- * Return: 0
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-	return(0);
+	return (0);
 }
