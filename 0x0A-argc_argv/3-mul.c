07d4a37e70df2cc8692fbdc5979076cb6f8c7903
Dinobi <architectstan01@gmail.com>
2022-11-05 05:19:28 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints out multiplication of two numbers
+ * @argc: integer
+ * @argv: string
+ * Return: 1 if program does not receive two arguments otherwise 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y, z;
+	
+	if (argc != 3)
+{		printf("Error\n");
+	return (1);
+}
+	else if (argc == 3)
+		x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	z = x * y;
+	printf("%i", z);
+	return (0);
+}
