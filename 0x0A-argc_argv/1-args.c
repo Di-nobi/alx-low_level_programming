731d568792a6927524b92f4907eba0a9367e0259
Dinobi <architectstan01@gmail.com>
2022-11-05 01:28:02 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints the number of arguments passed
+ * @argc: int 
+ * @argv: string
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %i", argc);
+	printf("\n");
+
+	return(0);
+}
