37e5368db9921ef8e5e50a2b331d882f4d000d88
Dinobi <architectstan01@gmail.com>
2022-11-11 20:26:39 -0800

    realoc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,6 +17,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 {
+	free(ptr);
+	return (NULL);
+}
+	if (ptr == NULL)
+{
 	ptr = malloc(new_size);
 	return (ptr);
 }
