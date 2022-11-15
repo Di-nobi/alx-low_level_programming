da31ba17f68d2002fcf7fe77dc4bd22e5dd5679d
Dinobi <architectstan01@gmail.com>
2022-11-15 12:01:14 -0800

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner:owner of dog
+ * Return: struct dog
+ * if fails, return NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p_dog;
+	int i, lname, lowner;
+
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(owner) || !(name))
+{
+	free(p_dog);
+	return (NULL);
+}
+	for (lname = 0; name[lname]; lname++);
+	for (lowner = 0; owner[lowner]; lowner);
+
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
+
+	if (!(p_dog->name) || !(p_dog->owner))
+{
+	free(p_dog->name);
+	free(p_dog->owner);
+	free(p_dog);
+	return (NULL);
+}
+	p_dog->age = age;
+
+	for (i = 0; i < lname; i++)
+	p_dog->name[i] = name[i];
+	name[i] = '\0';
+
+	for (i = 0; i < lowner; i++)
+	p_dog->owner[i] = owner[i];
+	owner[i] = '\0';
+	return (p_dog);
+}
