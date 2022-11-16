f3aa2a044515234fd31f990ac084bf804f7461e9
Dinobi <architectstan01@gmail.com>
2022-11-16 15:02:19 -0800

    calc

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,21 @@
+#ifndef PROJECT 3
+#define PROJECT 3
+/**
+ * struct operator - struct op
+ * @op: operator
+ * @f: function
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+}	op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
