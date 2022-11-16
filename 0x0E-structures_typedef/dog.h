3d23cd11ba2b0a2c2ec094971023c88d556a3596
Dinobi <architectstan01@gmail.com>
2022-11-16 06:51:24 -0800

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/*
+ * struct dog - structure definition of a dog
+ * @name: name string
+ * @age: integer
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}	dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
