a669e30cd77ee9251e1ec312e214ba5c896b5452
Dinobi <architectstan01@gmail.com>
2022-11-16 13:46:12 -0800

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int j;
 
 	j = 0;
-	if (array == NULL || !(size))
+	if (array == NULL || action == NULL)
 		return;
 	while (j < size)
 {
