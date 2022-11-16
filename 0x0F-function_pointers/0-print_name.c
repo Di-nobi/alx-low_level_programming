2c8379d99592c6ded33c7fa83a7459dfcc5bdea0
Dinobi <architectstan01@gmail.com>
2022-11-16 12:58:13 -0800

    updates

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: a name
+ * @d: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
