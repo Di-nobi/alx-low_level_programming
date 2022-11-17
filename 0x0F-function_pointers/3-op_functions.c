12c748ad6ed98cc4aa350dbca745e27ec1273769
Dinobi <architectstan01@gmail.com>
2022-11-16 20:31:23 -0800

    functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+/**
+ * op_add - Add two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ * Return: Integer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Sub two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ * Return: Integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ * Return: Integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - result of the division of two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ * Return: Integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - reminder of the division
+ * @a: Integer 1
+ * @b: Integer 2
+ * Return: Integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
