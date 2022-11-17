b122ce1164ab7058a21f51c17c83c3ffc8be6446
Dinobi <architectstan01@gmail.com>
2022-11-16 20:51:42 -0800

    funct

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,11 +7,13 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
+
 	op_t ops[] = 
 {
 	{"+", op_add},
 	{"-", op_sub},
-	{"*", op_mul}'
+	{"*", op_mul},
 	{"/", op_div},
 	{"%", op_mod},
 	{NULL, NULL},
