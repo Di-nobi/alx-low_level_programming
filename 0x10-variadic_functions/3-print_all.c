dcba639d896e89d72ff2990e376bd023296958ff
Dinobi <architectstan01@gmail.com>
2022-11-17 10:47:30 -0800

    all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,7 +18,31 @@ void print_all(const char * const format, ...)
 {
 	while (format[i])
 {
-	switch (format[i]){}
+	switch (format[i])
+{
+	case 'c':
+		printf("%s%c", sep, va_arg(list, int));
+		break;
+	case 'i':
+		printf("%s%d", sep, va_arg(list, int));
+		break;
+	case 'f':
+		printf("%s%f", sep, va_arg(list, double));
+		break;
+	case 's':
+		str = va_arg(list, char *);
+		if (!str)
+			str = "(nil)";
+		printf("%s%s", sep, str);
+		break;
+	default:
+		i++;
+		continue;
 }
-
+	sep = ",";
+	i++;
+}
+}
+	printf("\n");
+	va_end(list);
 }
