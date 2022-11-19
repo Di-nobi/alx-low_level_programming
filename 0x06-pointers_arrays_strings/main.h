54c3954f0eb41207f8bf2c6df81bf2ecaf7177b2
Dinobi <architectstan01@gmail.com>
2022-11-19 06:21:19 -0800

    redd

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,5 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
-char *infinite_add(char *n1, char *n2);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
