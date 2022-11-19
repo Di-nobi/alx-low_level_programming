7c359f3b871b12afea6dddd0a953690281d107a0
Dinobi <architectstan01@gmail.com>
2022-11-19 06:33:01 -0800

    adds

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -21,13 +21,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
+{
+	n = m;
+	if (i >= 0)
+		n += n1[i] - '0';
+	if (j >= 0)
+		n += n2[j] - '0';
+	if (i < 0 && j < 0 && n == 0)
 {
 	break;
 }
