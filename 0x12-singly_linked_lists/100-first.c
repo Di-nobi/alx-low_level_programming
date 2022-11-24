cea667be08182be6601a1083fe3f6b1597959e8b
Dinobi <architectstan01@gmail.com>
2022-11-24 14:33:19 -0800

    pros

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void msg(void)__attribute__((constructor));
+
+/**
+ * msg - prints a message
+ * Return: nothing
+ */
+
+void msg(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n);
+}
