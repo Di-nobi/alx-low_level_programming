a04a40dde797c806df17a3a098662f5d80eb1391
Dinobi <architectstan01@gmail.com>
2022-11-24 13:55:42 -0800

    lee

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list.
+ * print_list - prints all the elements of a list.
  * @h: head of a list.
- *
  * Return: numbers of nodes.
  */
+
 size_t print_list(const list_t *h)
 {
 	int x = 0;
