0cf96c61024ec4db0f7ea2e4eb891e6f7bbd27a0
Dinobi <architectstan01@gmail.com>
2022-11-25 02:40:14 -0800

    4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,9 +11,13 @@
 
 void free_list(list_t *head)
 {
-	if (head == 0)
-		return;
-	free_list(head->next);
-	free(head->str);
-	free(head);
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
 }
