38467e08045002feb812cf5dcd827648ba5835cd
Dinobi <architectstan01@gmail.com>
2022-11-28 09:38:38 -0800

    length_list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - a function that returns number of elements in a linked list
+ * @h: header
+ * Return: Number of elements ina linked listint_t list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (i);
+}
