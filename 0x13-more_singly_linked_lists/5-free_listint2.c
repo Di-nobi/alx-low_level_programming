5b5c3d20543ec5fdb2a685ab2d8b9cda27627919
Dinobi <architectstan01@gmail.com>
2022-11-29 07:13:00 -0800

    5-free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,11 +5,13 @@
  * free_listint2 - frees a linked list
  * @head: pointer to the listint_t list to be freed
  */
-
 void free_listint2(listint_t **head)
 {
 	listint_t *new;
 
+	if (head == NULL)
+		return;
+	
 	while (*head)
 {
 	new = (*head)->next;
