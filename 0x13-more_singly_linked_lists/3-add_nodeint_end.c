903f731c13f572c70d150964f996b018165995e3
Dinobi <architectstan01@gmail.com>
2022-11-29 06:45:24 -0800

    3-node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *tmp = *head;
+
 	new = malloc(sizeof(listint_t));
 	new->n = n;
 	new->next = NULL;
@@ -21,8 +22,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	return (new);
 }
 	while (tmp->next)
+{
 	tmp = tmp->next;
 
 	tmp->next = new;
 	return (new);
+}
+return (NULL);
 }
