3856c6290eab45194fd02c8c7e7e46dcbde483ec
Dinobi <architectstan01@gmail.com>
2022-11-29 07:15:30 -0800

    5-free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *new;
-
+	
 	if (head == NULL)
 		return;
 	
@@ -16,7 +16,7 @@ void free_listint2(listint_t **head)
 {
 	new = (*head)->next;
 	free(*head);
-	*head =new;
+	*head = new;
 }
 	*head = NULL;
 }
