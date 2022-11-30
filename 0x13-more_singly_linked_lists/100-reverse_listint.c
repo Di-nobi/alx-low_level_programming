ff35815fde845e414c840d4c5fd7f33e166483cb
Dinobi <architectstan01@gmail.com>
2022-11-29 17:55:31 -0800

    100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,12 +3,13 @@
 
 /**
  * reverse_listint - a function that reverses linked list
- * @*head: pointer to the first node in the list
+ * @head: pointer to the first node in the list
  * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prevnode, *curnode;
+
 	prevnode = NULL;
 	curnode = NULL;
 	
@@ -19,6 +20,6 @@ listint_t *reverse_listint(listint_t **head)
 	prevnode = *head;
 	*head = curnode;
 }
-*head = prevnode;
-return (*head);
+	*head = prevnode;
+	return (*head);
 }
