b4487802693119f6da4e90e7640a8b9e2f495b7c
Dinobi <architectstan01@gmail.com>
2022-12-05 12:30:53 -0800

    files2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, p, d;
+
+	if (filename == 0)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if ( fd == -1)
+		return (-1);
+
+	if (text_content != 0)
+{
+	for (d = 0; text_content[d] != '\0'; d++)
+		;
+	p = write(fd, text_content, d);
+
+	if (p < 0)
+		return (-1);
+}
+
+close(fd);
+
+return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 
 int _putchar(char c);
@@ -9,4 +13,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#end
+#endif
