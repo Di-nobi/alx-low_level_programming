5fff27a6e5fcd72caf440cf3fc7d96675062b1e8
Dinobi <architectstan01@gmail.com>
2022-12-05 12:42:16 -0800

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,17 +17,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
         if (!filename)
                 return (0);
+
         f = malloc(sizeof(char) * letters);
         if (!f)
                 return (0);
 	
 	fd = open(filename, O_RDONLY);
+
         if (fd == -1)
 {
 	free(f);
         return (0);
 }
         n = read(fd, f, letters);
+
         if (n == -1)
 {
         free(f);
@@ -36,14 +39,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
         f[n] = '\0';
 
         p = write(STDOUT_FILENO, f, n);
+
         if (p == -1)
 {
         free(f);
         return (0);
 }
 
-
-        free(f);
+	free(f);
         close(fd);
         return (p);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if ( fd == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != 0)
