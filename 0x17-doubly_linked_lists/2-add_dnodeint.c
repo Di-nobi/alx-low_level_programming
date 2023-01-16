0313bd8ec0cd3bb9e207fc74171454063a01d265
Dinobi <architectstan01@gmail.com>
2023-01-16 06:07:27 -0800

    2nodes

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/*
+ * add_node - adds a new node at the beginning of a dlistint_t list
+ * @n: integer
+ * @head: head
+ * Return: Address of new element, or NULL if failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (tmp)
+	{
+		tmp->n = n;
+		tmp->next = *head;
+		*head = tmp
+			return (tmp);
+	}
+	else
+		return (NULL);
+}
