170e1c908f60fa471125fe8448e8ff95ac912609
Dinobi <architectstan01@gmail.com>
2023-01-16 06:44:13 -0800

    addnode

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		tmp->prev = NULL;
 		if (*head)
 		{
-			*head->prev =tmp;
+			(*head)->prev =tmp;
 		}
 		*head = tmp;
 			return (tmp);
