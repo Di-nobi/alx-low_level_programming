d2c6cb6b7c26ac98280f9c9f615d8aa504cf9ec8
Dinobi <architectstan01@gmail.com>
2023-01-17 09:14:43 -0800

    updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -14,8 +14,8 @@
 typedef struct dlistint_s
 {
 	int n;
-	struct dlistint *prev;
-	struct dlistint *next;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
