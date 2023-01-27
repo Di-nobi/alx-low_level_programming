baa7345226ef35713d3aa100ea8a66d7e1fc002e
Dinobi <architectstan01@gmail.com>
2023-01-27 13:12:01 -0800

    3-cp_update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,8 +19,10 @@ int main(int argc, char *argv[])
         char buff[1024];
 
         if (argc != 3)
+	{
                 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
+		exit(97);
+	}
 
         file_from = open(argv[1], O_RDONLY);
         if (file_from == -1)
@@ -31,8 +33,10 @@ int main(int argc, char *argv[])
 
         file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
+{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
+}
 	while ((rcheck = read(file_from, buff, 1024)) > 0)
 {
 	wcheck = write(file_to, buff, rcheck);
@@ -40,7 +44,7 @@ int main(int argc, char *argv[])
 		if (wcheck != rcheck)
         {
 		      dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
-                exit(99);
+		      exit(99);
         }
 }
 
