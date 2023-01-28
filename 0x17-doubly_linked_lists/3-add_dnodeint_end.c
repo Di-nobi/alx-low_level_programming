aa150700822119c243df71ab8ca146b06f418d26
Dinobi <architectstan01@gmail.com>
2023-01-27 17:53:31 -0800

    updates

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,27 +1,39 @@
 #include "lists.h"
+
 /**
- * add_node_end - adds a new node at the end of a dlistint_t list
- * @head: pointing to a doubly linked list
- * @n: integer
- * Return: address of the new element or NULL if it failed
+ * add_dnodeint_end - adds a new node at the end of a doubly linked list
+ * @head: double pointer to the list
+ * @n: data to insert in the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
-	
-	if (newNode)
-	{
-		newNode->n = n;
-		newNode->next = NULL;
-        
-	while ((*head)->next)
-	{
-		*head = (*head)->next;
+	dlistint_t *new;
+	dlistint_t *temp = *head;
 
-		(*head)->next = newNode;
-		newNode->prev = *head;
-	}
-	}
-	else
+	if (!head)
 		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+	new->prev = temp;
+
+	return (new);
 }
